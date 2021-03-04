a151be734c9483432ab13251fb0c6578478b91fd
Riazur-Rahim <79905316+Riazur-Rahim@users.noreply.github.com>
2021-03-04 23:55:44 +0530

    Add files via upload

---
+++ Embedded_C/8051/6.two_seven_seg.c
@@ -0,0 +1,39 @@
+#include<reg51.h>
+sbit Q1=P1^4;
+sbit Q2=P1^5;
+void delay(unsigned int);
+void main()
+{
+												//0		 1		2			3		4			5		6			7		8			9		A			b		C			d		E		F
+	unsigned char seg[16]={0x3F,0x06,0x5B,0x4F,0x66,0x6D,0x7D,0x07,0x7F,0x6F,0x77,0x7C,0x39,0x5E,0x79,0x71};
+	
+	while(1)
+	{
+		unsigned int i,j,k;
+		for(i=0;i<=16;i++)
+		{
+			for(j=0;j<=16;j++)
+			{
+				for(k=0;k<1000;k++)
+				{
+					Q1=1;
+					Q2=0;
+					P3=seg[j];
+					delay(1);
+					Q1=0;
+					Q2=1;
+					P3=seg[i];
+					delay(1);
+				}
+			}
+		}
+	}
+}
+void delay(unsigned int k)
+{
+	unsigned int x,y;
+	for(x=0;x<k;x++)
+	{
+		for(y=0;y<50;y++);
+	}
+}

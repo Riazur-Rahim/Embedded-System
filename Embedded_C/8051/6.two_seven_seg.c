84551f2414789451dcfc52755ed9cf32d8aace3c
Riazur-Rahim <79905316+Riazur-Rahim@users.noreply.github.com>
2021-03-07 23:09:45 +0530

    Update 6.two_seven_seg.c

--- Embedded_C/8051/6.two_seven_seg.c
+++ Embedded_C/8051/6.two_seven_seg.c
@@ -1,18 +1,18 @@
 #include<reg51.h>
-sbit Q1=P1^4;
-sbit Q2=P1^5;
+sbit Q1=P1^0;
+sbit Q2=P1^1;
 void delay(unsigned int);
 void main()
 {
-										
-	unsigned char seg[16]={0x3F,0x06,0x5B,0x4F,0x66,0x6D,0x7D,0x07,0x7F,0x6F,0x77,0x7C,0x39,0x5E,0x79,0x71};
+							
+	unsigned char seg[10]={0x3F,0x06,0x5B,0x4F,0x66,0x6D,0x7D,0x07,0x7F,0x6F};//,0x77,0x7C,0x39,0x5E,0x79,0x71};
 	
 	while(1)
 	{
 		unsigned int i,j,k;
-		for(i=0;i<=16;i++)
+		for(i=0;i<=9;i++)
 		{
-			for(j=0;j<=16;j++)
+			for(j=0;j<=9;j++)
 			{
 				for(k=0;k<1000;k++)
 				{
@@ -20,10 +20,13 @@ void main()
 					Q2=0;
 					P3=seg[j];
 					delay(1);
+							
 					Q1=0;
 					Q2=1;
 					P3=seg[i];
 					delay(1);
+				
+			
 				}
 			}
 		}

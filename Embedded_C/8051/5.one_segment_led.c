eca20a0ce2713000b7333f7670544bb9bf4cef8a
Riazur-Rahim <79905316+Riazur-Rahim@users.noreply.github.com>
2021-03-07 23:01:01 +0530

    Update 5.one_segment_led.c

--- Embedded_C/8051/5.one_segment_led.c
+++ Embedded_C/8051/5.one_segment_led.c
@@ -1,7 +1,7 @@
 #include<reg51.h>
 void delay(unsigned int);
 void main()
-{						//0			1		2			3		4			5		6			7		8			9		A			b		C			d		E			F
+{			       //0    1	  2	3   4  	 5    6    7    8    9    A    b    C    d    E    F
 	unsigned char seg[16]={0x3F,0x06,0x5B,0x4F,0x66,0x6D,0x7D,0x07,0x7F,0x6F,0x77,0x7C,0x39,0x5E,0x79,0x71};
 	unsigned int i;
 	while(1)
